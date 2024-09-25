451ae4b9c8337075408de0675bc933986b2e828f
Jonatan García <jntngf@gmail.com>
2024-09-25 14:40:01 +0100

    Add message properties and adapt to latest changes in common lib

--- src/main/java/net/portic/processdirector/ics2/prepare/ICS2Preparer.java
+++ src/main/java/net/portic/processdirector/ics2/prepare/ICS2Preparer.java
@@ -1,20 +1,22 @@
 package net.portic.processdirector.ics2.prepare;
 
 import lombok.extern.slf4j.Slf4j;
-import net.portic.processdirector.common.domain.stages.prepare.Preparer;
+import net.portic.processdirector.common.domain.model.ExecutionContext;
+import net.portic.processdirector.common.domain.service.ConfigurationService;
+import net.portic.processdirector.common.domain.service.stages.prepare.Preparer;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class ICS2Preparer extends Preparer {
 
-    @Override
-    public boolean supports() {
-        return true;
+    public ICS2Preparer(ConfigurationService configurationService) {
+        super(configurationService);
     }
 
     @Override
-    public void execute() {
-        log.info("Preparing with the ICS2 preparer ...");
+    public ExecutionContext execute(ExecutionContext executionContext) {
+        log.info("Preparing with ICS2 preparer ...");
+        return executionContext;
     }
 }
--- src/test/java/net/portic/processdirector/ics2/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package net.portic.processdirector.ics2;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
